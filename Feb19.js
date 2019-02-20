b720f616799b93e03f4a9374caa131ee4a4557a4
Greg Roques <greg.roques@gmail.com>
2019-02-20 13:12:27 -0500

    new solution

--- Feb19.js
+++ Feb19.js
@@ -1,38 +1,38 @@
-// const str1="Hello"
-// const str2= 'Wolrd'
-// let matches=[]
+let str1="Hello"
+let str2= 'Wolrd'
+let matches=[]
 
-// function twoStrings(string1,string2){
-//     for(let i=0;i<string1.length;i++){
-//         for (let j=0;j<string2.length;j++){
-//             if(string1[i] === string2[j]){
-//                 matches.push(string1[i])
-//                 string2 = string2.replace('string2[j]', '')
-//                 break 
-//             }    
-//         }
-       
-//     }
-//     console.log(matches);
-//     console.log(matches.length);
-// }
-// twoStrings(str1,str2)
-
-function twoStrings(s1, s2) {
-    let alpha = "abcdefghijklmnopqrstuvwxyz";
-    let substring = false;
-    for (let i = 0; i < alpha.length; i++) {
-        let regexCheck = RegExp(`[${alpha[i]}]+`)
-        let match1 = regexCheck.test(s1);
-        let match2 = regexCheck.test(s2);
-        if (match1 == true && match2 == true) {
-            substring = true;
+function twoStrings(string1,string2){
+    for(let i=0;i<string1.length;i++){
+        for (let j=0;j<string2.length;j++){
+            if(string1[i] === string2[j]){
+                matches.push(string1[i])
+                string2 = string2.replace('string2[j]', '')
+                i++
+            }    
         }
+       
     }
-    if (substring == true) {
-        return ("YES")
-    } else {
-        return ("NO")
-    }
+    console.log(matches);
+    console.log(matches.length);
 }
+twoStrings(str1,str2)
+
+// function twoStrings(s1, s2) {
+//     let alpha = "abcdefghijklmnopqrstuvwxyz";
+//     let substring = false;
+//     for (let i = 0; i < alpha.length; i++) {
+//         let regexCheck = RegExp(`[${alpha[i]}]+`)
+//         let match1 = regexCheck.test(s1);
+//         let match2 = regexCheck.test(s2);
+//         if (match1 == true && match2 == true) {
+//             substring = true;
+//         }
+//     }
+//     if (substring == true) {
+//         return ("YES")
+//     } else {
+//         return ("NO")
+//     }
+// }
 
