4418b8e08fbfb93c5cb568ced29adfb52df72299
Greg Roques <greg.roques@gmail.com>
2019-03-16 16:06:31 -0400

    questions

---
+++ UIchallenge/js.js
@@ -0,0 +1,48 @@
+// // ============================================================== Question 1
+
+// function log(level,color){
+//     let logLevel = {}
+//    if (level.length === color.length){
+//        for (i=0;i<=log.length;i++){
+//             let key = level[i]
+//             let value = color[i]
+//             logLevel[key] = value
+//         }
+
+//     function returnMessage(dict,text){
+//         for (var key in dict){
+//             let regEx = new RegExp(key)
+//             if (regEx.test(text)){
+//                 document.getElementById('result').innerHTML=`
+//                 <p style="color:${dict[key]}">
+//                 [${key}] ${text}
+//                 </p>
+//                 `
+//             }
+//         }
+//     }
+//     // const text = "Here is some info"
+//     // const text = "This is a warning"
+//     const text = "This is my debug message"
+//     returnMessage(logLevel, text)
+//     }
+// }
+
+// log (['debug','warn','info'], ['orange','red','green'])
+
+// ====================================== Question 2
+
+
+
+// const timer = ms => logName => 
+
+// setTimeout(console.log, ms, `${logName} called`);
+
+const log1 = timer(2500);
+console.log(log1)
+const log2 = timer(1500);
+const log3 = timer(500);
+
+
+
+
