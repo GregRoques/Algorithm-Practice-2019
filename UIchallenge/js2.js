64b53625c7bcebd0674beaee92ce7e3c6e07e949
Greg Roques <greg.roques@gmail.com>
2019-03-17 10:02:31 -0400

    changes

---
+++ UIchallenge/js2.js
@@ -0,0 +1,24 @@
+// ====================================== Question 2
+
+
+let i = 1
+async function timer(ms){
+  
+  let promise = new Promise ((resolve,reject)=>{
+    setTimeout(()=> resolve(`Log${i} (${ms} ms) called`), ms)
+  })
+
+  let result = await promise
+  
+  console.log(result)
+}
+
+async function log(){
+    const log1 = await timer(2500);
+    i++;
+    const log2 = await timer(1500);
+    i++
+    const log3 = await timer(500);
+}
+
+log();
