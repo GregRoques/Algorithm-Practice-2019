5a8d8b70b3c50a4e8b226ea41dbb46ce27296c0f
Greg Roques <greg.roques@gmail.com>
2019-03-17 19:57:20 -0400

    challenge complete

--- UIchallenge/js1.js
+++ UIchallenge/js1.js
@@ -1,32 +1,32 @@
 //  ============================================================== Question 1
 
 function log(level,color){
-    let logLevel = {}
+    let logLevel = {};
    if (level.length === color.length){
        for (i=0;i<=log.length;i++){
-            let key = level[i]
-            let value = color[i]
-            logLevel[key] = value
+            let key = level[i];
+            let value = color[i];
+            logLevel[key] = value;
         }
 
     function returnMessage(dict,text){
         for (var key in dict){
-            let regEx = new RegExp(key)
+            let regEx = new RegExp(key);
             if (regEx.test(text)){
                 document.getElementById('result').innerHTML=`
                 <p style="color:${dict[key]}">
                 [${key}] ${text}
                 </p>
-                `
+                `;
             }
         }
     }
     // const text = "Here is some info"
     // const text = "This is a warning"
-    const text = "This is my debug message"
-    returnMessage(logLevel, text)
+    const text = "This is my debug message";
+    returnMessage(logLevel, text);
     }
 }
 
-log (['debug','warn','info'], ['orange','red','green'])
+log (['debug','warn','info'], ['orange','red','green']);
 
--- UIchallenge/js2.js
+++ UIchallenge/js2.js
@@ -1,24 +1,26 @@
-// ====================================== Question 2
+// // ====================================== Question 2
 
 
-let i = 1
+let i = 1;
 async function timer(ms){
   
   let promise = new Promise ((resolve,reject)=>{
     setTimeout(()=> resolve(`Log${i} (${ms} ms) called`), ms)
-  })
+  });
 
-  let result = await promise
+  let result = await promise;
   
-  console.log(result)
+  console.log(result);
 }
 
 async function log(){
     const log1 = await timer(2500);
     i++;
     const log2 = await timer(1500);
-    i++
+    i++;
     const log3 = await timer(500);
 }
 
 log();
+
+
