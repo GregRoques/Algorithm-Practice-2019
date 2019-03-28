819852a3c92671b8059ff200b8fa42c14080b15b
Greg Roques <greg.roques@gmail.com>
2019-03-28 15:26:19 -0400

    practice with Big O Notation

--- BigO/part1.js
+++ BigO/part1.js
@@ -4,3 +4,36 @@ const reverseString = aString => {
    console.log( aString.split("").reverse().join(""))
 }
 reverseString('doggy')
+
+// ___________________________________________________________________
+
+// What is better code?
+   // - faster and less-memory
+
+      // You can use timers to achieve this:
+   
+   // ex:
+   // use in browser – Google Chrome Source
+
+   const addUpTo = n =>{
+      let total = 0;
+      for (let i = 1; i <= n; i++){
+         total += i;
+      }
+      console.log(total)
+   }
+
+   // var t1 = performance.now();
+   addUpTo(100);
+   // var t2 = performance.now();
+   
+   // console.log(`Time Elapsed ${(t2 - t1) / 1000} seconds.`)
+
+      // ================================== This said...the problem with time
+         // 1. Different machines will have different times
+         // 2. The same machine will have different times
+         // 3. For fast algorithms, speed measurements may not be precise enough
+
+// ___________________________________________________________________
+
+   // Big O Notation – Discusses how the runtime of an algorithm grows as its input grows.
