e96d7d0d4487d5e88ef073d909b20e39454afbfd
Greg Roques <greg.roques@gmail.com>
2019-03-28 16:38:27 -0400

    logarithm intro

--- BigO/part1.js
+++ BigO/part1.js
@@ -37,3 +37,39 @@ reverseString('doggy')
 // ___________________________________________________________________
 
    // Big O Notation – Discusses how the runtime of an algorithm grows as its input grows.
+
+   //  O(1) - constant; no change in run time
+   //  O(n) - linear growth; grows at same rate as n ...loops
+   //  O(n^2) – quadratic; squares in relation to n ... nested loops
+
+   // ============= Some Rules:
+
+   // Constants:
+      // 1. Arithmetic operations are constant
+      // 2. variable assignments are constant
+      // 3. Accessing elements in an array (by index or object key) is constant
+
+      // 4.In a loop, the complexity of the loop is the length of the loop times the complexity
+         // of whatever is happening inside the loop
+
+// ==================== Space Complexity (or memory)
+
+      // "auxilary space" refers to space taken up by the algorithm, not by the inputs
+      // Space complexity as it relates to auxillary space deals with the inputs
+
+      // JS Space Complexity Rules of Thumb:
+         // 1.  Most primitives (booleans, numbers, undefined, null) are constant space
+         // 2. Strings require O(n) space (where n is the length)
+         // 3. Reference types are generally O(n), where n is the length (for arrays) or the number of keys (for obects)
+
+// ___________________________________________________________________
+
+// Logarithms
+   // the inverse of exponents(also known as 'to the power of'...2^3...the superscript)
+   // we just use the shorthand "log"...not log + subscript
+
+   // put another way:
+      // The logarithm of a number roughly measures the number of times you can divide that number by 2
+         // before you get a value that's less than or equal to one.
+
+         // so... log(8) = 3; or, 8 /2 = 4/2 = 2/2 =1 ...divisible by 2 three times
